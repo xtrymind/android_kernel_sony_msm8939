b3ba5c6b8ed85b459443ef096658586ae7806b5f
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2016-12-21 11:32:13 +0100

    26.3.A.1.33
    Open source code for
    Xperia M4 Aqua (E2303, E2353, E2306)
    Xperia M4 Aqua Dual (E2333, E2363, E2312)
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>
    Signed-off-by: Dede Dindin Qudsy <xtrymind@gmail.com>

--- drivers/base/regmap/regmap.c
+++ drivers/base/regmap/regmap.c
@@ -17,6 +17,9 @@
 #include <linux/err.h>
 #include <linux/rbtree.h>
 #include <linux/sched.h>
+//CORE-KC-FixDeviceCanNotPowerON-00 +[
+#include <linux/delay.h>
+//CORE-KC-FixDeviceCanNotPowerON-00 +]
 
 #define CREATE_TRACE_POINTS
 #include <trace/events/regmap.h>
@@ -1670,10 +1673,15 @@ static int _regmap_bus_read(void *context, unsigned int reg,
 	return ret;
 }
 
+//CORE-KC-FixDeviceCanNotPowerON-00 +[
+#define REG_NCP6335D_PROGVSEL0      0x11
+#define NCP6335D_ENABLE             BIT(7)
+//CORE-KC-FixDeviceCanNotPowerON-00 +]
 static int _regmap_read(struct regmap *map, unsigned int reg,
 			unsigned int *val)
 {
 	int ret;
+
 	void *context = _regmap_map_get_context(map);
 
 	WARN_ON(!map->reg_read);
@@ -1688,6 +1696,25 @@ static int _regmap_read(struct regmap *map, unsigned int reg,
 		return -EBUSY;
 
 	ret = map->reg_read(context, reg, val);
+//CORE-KC-FixDeviceCanNotPowerON-00 +[
+	if (reg == REG_NCP6335D_PROGVSEL0 && !(*val & NCP6335D_ENABLE)) {
+		int num = 0;
+		do {
+			if (!num)
+				pr_err("ncp6335d: BAD VALUE! reg=%x val=%x ret=%d\n",
+					reg, *val, ret);
+			ret = map->reg_read(context, reg, val);
+			pr_info("ncp6335d: RETRY[%d] reg_read reg=%x val=%x ret=%d\n",
+				num, reg, *val, ret);
+			msleep(10);
+			num++;
+		} while (!(*val & NCP6335D_ENABLE) && num < 500);
+                if (!(*val & NCP6335D_ENABLE)) {
+                        panic("NCP6335D_ENABLE is gone");
+                        return ret;
+                }
+	}
+//CORE-KC-FixDeviceCanNotPowerON-00 +]
 	if (ret == 0) {
 #ifdef LOG_DEVICE
 		if (map->dev && strcmp(dev_name(map->dev), LOG_DEVICE) == 0)
